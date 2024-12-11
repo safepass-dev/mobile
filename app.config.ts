1ad906184b81235dd4d7d9002742e913009759c9
atillaturker <90347329+atillaturker@users.noreply.github.com>
2024-12-12 01:27:08 +0300

    feat: typescript migration & alias

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+import "ts-node/register"; // Add this to import TypeScript files
+import { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "safepass",
+  slug: "safepass",
+};
+
+export default config;
