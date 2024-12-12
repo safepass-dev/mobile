5b53b3d3ebaabb461d70ca6c83c277dddc8aa714
atillaturker <90347329+atillaturker@users.noreply.github.com>
2024-12-12 21:02:32 +0300

    misc:expo native config

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,13 @@
-import "ts-node/register"; // Add this to import TypeScript files
 import { ExpoConfig } from "expo/config";
+import "ts-node/register"; // Add this to import TypeScript files
 
 const config: ExpoConfig = {
+  android: {
+    package: "com.anonymous.safepass",
+  },
+  ios: {
+    bundleIdentifier: "com.anonymous.safepass",
+  },
   name: "safepass",
   slug: "safepass",
 };
